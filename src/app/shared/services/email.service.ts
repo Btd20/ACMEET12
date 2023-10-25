c02c4e3a7154b01e8cacb3739427cc79eb74ccf9
LaiaRodriguez22 <lrodrigu22@jaumebalmes.net>
2023-10-25 10:22:39 +0200

    Ruta relativa email

--- src/app/shared/services/email.service.ts
+++ src/app/shared/services/email.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environments';
-import { Booking } from 'src/app/interfaces/booking';
+import { Booking } from '../interfaces/booking';
 
 @Injectable({
   providedIn: 'root'
