d981e4dfd7aabe3e03bd0fcbd6f9a2e6c892d591
LaiaRodriguez22 <lrodrigu22@jaumebalmes.net>
2023-10-27 10:42:50 +0200

    Refactor the footer, showing Vero and Gemma hehe.

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production:false,
     endpoint: {
-      users: 'https://localhost:7190/',
+      users: 'https://localhost:7136/',
       geolocalizaciones: 'https://localhost:7099/',
       reservas: 'https://localhost:7038/',
       emails: 'https://localhost:7136/'
