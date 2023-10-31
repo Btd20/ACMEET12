842d361d374dac8e54b42d319c13656e16ea392e
LaiaRodriguez22 <lrodrigu22@jaumebalmes.net>
2023-10-31 09:48:37 +0100

    Buttons now with change color into hover and active -bug fixed-

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    production:false,
-    endpoint: {
-      users: 'https://localhost:7136/',
-      geolocalizaciones: 'https://localhost:7099/',
-      reservas: 'https://localhost:7038/',
-      emails: 'https://localhost:7136/'
-    }
+  production:false,
+  endpoint: {
+    users: 'https://localhost:7190/',
+    geolocalizaciones: 'https://localhost:7099/',
+    reservas: 'https://localhost:7038/',
+    emails: 'https://localhost:7136/'
+  }
 
 };
