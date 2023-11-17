5203f49d84dd38521f21872b1f81b6d438257aa7
Eric <eric.ramos.soto@gmail.com>
2023-11-17 09:49:03 +0100

    .

---
+++ Help/Controllers/TicketController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ticket.Controllers
+{
+    [Route("api/tickets")]
+    public class TicketsController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult CreateTicket(TicketsController ticket)
+        {
+            _questionsRepository.Add(ticket);
+            _questionsRepository.SaveChanges();
+
+            return Ok();
+        }
+    }
+
+    [Route("api/questions")]
+    public class QuestionsListController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetQuestions()
+        {
+            return Ok(_questionsRepository.GetAll());
+        }
+    }
+
+    [Route("api/questions/{id}")]
+    public class QuestionController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetQuestion(int id)
+        {
+            return Ok(_questionsRepository.GetById(id));
+        }
+    }
+
+    [Route("api/questions/{id}/answer")]
+    public class AnswerController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult CreateAnswer(int id, AnswerController answer)
+        {
+            var question = _questionsRepository.GetById(id);
+            question.Answers.Add(answer);
+            _questionsRepository.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Help/Models/TicketDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Sockets;
+using System.Reflection.Emit;
+
+namespace Ticket.Models
+{
+    public class TicketDbContext : DbContext
+    {
+        public TicketDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\mssqllocaldb;Database=MyApp;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ticket>().ToTable("Tickets");
+        }
+    }
+}
