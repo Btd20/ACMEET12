201ec0ef1a493029ec9d9b4f90666da4afecb5f8
yeray.lorenzo <bergahd@gmail.com>
2023-12-01 13:30:04 +0100

    IS

---
+++ src/app/auth/auth-config.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { AuthModule } from 'angular-auth-oidc-client';
+
+
+@NgModule({
+    imports: [AuthModule.forRoot({
+        config: {
+            authority: 'https://localhost:44310',
+            redirectUrl: 'http://localhost:4200/home',
+            postLogoutRedirectUri: window.location.origin,
+            clientId: 'acme_web',
+            scope: 'openid', // 'openid profile ' + your scopes
+            responseType: 'code',
+            silentRenew: true,
+            useRefreshToken: true,
+            silentRenewUrl: window.location.origin + '/silent-renew.html',
+            renewTimeBeforeTokenExpiresInSeconds: 30,
+        }
+      })],
+    exports: [AuthModule],
+})
+export class AuthConfigModule {}
